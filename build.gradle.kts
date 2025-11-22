38ed16e014f2eba5bf978ffd39cb3ccfb9850473
sammie7337 <samuelwainaina13@gmail.com>
2025-11-22 09:42:09 +0300

    Add clean task to build.gradle.kts
    Added a clean task to delete the build directory.

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,13 +3,13 @@ buildscript {
         google()
         mavenCentral()
     }
-    dependencies {
-        classpath("com.android.tools.build:gradle:8.2.2")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.20")
-    }
 }
 
 plugins {
     id("com.android.application") version "8.2.2" apply false
     id("org.jetbrains.kotlin.android") version "1.9.20" apply false
+}
+
+tasks.register("clean", Delete::class) {
+    delete(rootProject.layout.buildDirectory)
 }
